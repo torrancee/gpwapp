17c72db276aa05f9fbca4802c25c4852febdb4f4
Wiktor <torrancee@gmail.com>
2017-05-24 00:09:06 +0200

    small changes

--- gpw_project/gpw_app/mainwindow.cpp
+++ gpw_project/gpw_app/mainwindow.cpp
@@ -12,3 +12,8 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButtonAkrz_clicked()
+{
+    ui->pushButtonAkrz->setText("GPW");
+}
--- gpw_project/gpw_app/mainwindow.h
+++ gpw_project/gpw_app/mainwindow.h
@@ -15,6 +15,10 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_pushButtonAkrz_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
